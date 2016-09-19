689e3932b673db805712d62370331da4d446556b
Andrew Savelev <savelevcorr@yandex.ru>
2016-09-19 22:37:58 +0300

    Создан Topic-list.jsx component

---
+++ src/components/Topic-list.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class TopicLIst extends Component {
+	render() {
+		return (
+			<section className="list-group">
+				Topic List
+			</section>
+		);
+	}
+}
