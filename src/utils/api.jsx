4c83758112e47392372ec3ad849f4fc74cd14e57
Andrew Savelev <savelevcorr@yandex.ru>
2016-09-20 16:49:02 +0300

    fix api.jsx

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -3,10 +3,12 @@ import Fetch from 'whatwg-fetch';
 const ROOT_URL = 'https://api.imgur.com/3/';
 const API_KEY = 'bb59f3b3367ff02';
 
-export default function getData (sUrl) {
+let getData = function (sUrl) {
 	return fetch(ROOT_URL + sUrl, {
 		headers: {
 			'Authorization': `Client-ID ${API_KEY}`
 		}
 	}).then((response) => response.json());
-}
+};
+
+export default getData;
