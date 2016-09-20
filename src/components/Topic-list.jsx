8107f55cdda19ac1accd1c2eadf3d64fe5d96e90
Andrew Savelev <savelevcorr@yandex.ru>
2016-09-20 16:47:47 +0300

    Topic-list.jsx - отображаем коллекцию топиков

--- src/components/Topic-list.jsx
+++ src/components/Topic-list.jsx
@@ -1,11 +1,54 @@
 import React, {Component} from 'react';
+import {Router, Link} from 'react-router';
+import TopicStore from '../stores/topic-store';
+import Actions from '../actions';
 
-export default class TopicLIst extends Component {
+export default class TopicList extends Component {
+	constructor(props) {
+		super(props);
+        this.state = {
+        	topics: []
+        };
+    }
+
+	componentWillMount() {
+		Actions.getTopics();
+	}
+
+	componentDidMount() {
+		this.unsubscribe = TopicStore.listen(this.onChange.bind(this));
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
+
+	onChange(topics) {
+		this.setState({
+			topics: topics
+		});
+	}
+
 	render() {
 		return (
-			<section className="list-group">
-				Topic List
-			</section>
+			<div className="page">
+				<h1>Topic List</h1>
+
+				<section className="list-group">
+					{this.renderTopics()}
+				</section>
+			</div>
 		);
+	}
+
+	renderTopics() {
+		return this.state.topics.map((topic) => {
+			return (
+				<Link key={topic.id} className="list-group-item">
+					<h3 className="list-group-item-heading">{topic.name}</h3>
+					<p className="list-group-item-text">{topic.description}</p>
+				</Link>
+			)
+		});
 	}
 }
