f3c9ff100137216a71f8927d7655b36edf62d235
Andrew Savelev <savelevcorr@yandex.ru>
2016-09-22 13:56:16 +0300

    Topic.jsx - реализована логика отображения картинок

--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -1,9 +1,51 @@
 import React, {Component} from 'react';
+import ImageStore from '../stores/image-store';
+import Actions from '../actions';
 
 export default class Topic extends Component {
+	constructor(props) {
+		super(props);
+        this.state = {
+        	images: []
+        };
+    }
+
+  componentWillMount() {
+		Actions.getImages(this.props.params.id);
+	}
+
+	componentDidMount() {
+		this.unsubscribe = ImageStore.listen(this.onChange.bind(this));
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
+
+	compoentWillReciveProps(nextProps) {
+		Actions.getImages(nextProps.params.id);
+	}
+
+	onChange(images) {
+		console.log("images are", images);
+		this.setState({
+			images: images
+		});
+	}
+
+	createImage() {
+		return this.state.images.map((image) => {
+			if (!image.is_album) {
+				return <img src={image.link} width={image.width} heigth={image.height} />
+			}
+		})
+	}
+
 	render() {
 		return (
-			<h1>I'm a some topic</h1>
+			<div>
+				{this.createImage()}
+			</div>
 		);
 	}
 }
